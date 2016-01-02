9b58fcf53b9d0a17773600294ee12bb2de35253c
Andrew Scott <amscott@openmailbox.org>
2016-01-02 17:11:52 +0000

    Adds messenger

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -1,8 +1,11 @@
 defmodule HelloPhoenix.HelloController do
-  use HelloPhoenix.web, :controller
+  use HelloPhoenix.Web, :controller
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
+  def show(conn, %{"messenger" => messenger}) do
+    render conn, "show.html", messenger: messenger
+  end
 end
